685bc82d8f4a9782503b90594836a93ebf82f60a
jasonmunro <jason@hastymail.org>
2014-02-03 10:35:39 -0600

    add examples of the cache usage

--- examples.php
+++ examples.php
@@ -10,9 +10,10 @@
  * and tweak the server/port/etc/ for your particular setup.
  */
 
-define( 'ALL_COMMANDS_EXAMPLE', true ); /* runs every public method in the class */
-define( 'NEW_MESSAGE_CHECK', true );    /* gets the subjects of the 5 newest messages in each mailbox */
-define( 'INBOX_LISTING', true );        /* gets the first 100 messages by arrival time and prints their headers */
+define( 'INBOX_LISTING',        true ); /* gets the first 10 messages by arrival time and prints their headers */
+define( 'NEW_MESSAGE_CHECK',    true ); /* gets the subjects of the 5 newest messages in each mailbox */
+define( 'ALL_COMMANDS_EXAMPLE', true ); /* runs all the IMAP commands in the class */
+define( 'CACHE_USE_EXAMPLE',    true ); /* uses the built in caching system */
 
 /* include IMAP library */
 require('hm-imap.php');
@@ -48,10 +49,11 @@ if ($imap->connect([
     'read_only'      => true,         // read only IMAP connection
     'search_charset' => false,        // charset for searching, can be US-ASCII, UTF-8, or false
     'sort_speedup'   => true,         // use non-compliant fast sort order processing
+    'use_cache'      => false,        // use a built in cache to reduce IMAP server activity
     'folder_max'     => 500 ])) {     // maximum number of mailboxes to fetch in get_mailbox_list()
 
 
-    /* display the first 100 message headers in the inbox */
+    /* display the first 10 message headers in the inbox */
     if ( INBOX_LISTING ) {
 
         /* select the INBOX */
@@ -66,8 +68,8 @@ if ($imap->connect([
             /* if the INBOX is not empty continue */
             if ( ! empty( $uids ) ) {
 
-                /* get the list of header values for the first 100 UIDs */
-                $msg_headers = $imap->get_message_list( array_slice( $uids, 0, 100 ) );
+                /* get the list of header values for the first 10 UIDs */
+                $msg_headers = $imap->get_message_list( array_slice( $uids, 0, 10 ) );
 
                 /* dump the headers */
                 print_r( $msg_headers );
@@ -182,11 +184,52 @@ if ($imap->connect([
         }
     }
 
+    /* use the cache related functions */
+    if ( CACHE_USE_EXAMPLE ) {
+
+        /* turn the cache on */
+        $imap->use_cache = true;
+
+        /* select the INBOX */
+        if ( $imap->select_mailbox( 'INBOX' )['selected'] ) {
+
+            /* fetch the UIDs, this result should be cached */
+            $imap->get_message_uids();
+
+            /* dump the cache into a local variable */
+            $cache = $imap->dump_cache();
+
+            /* clear current cache */
+            $imap->bust_cache( 'ALL' );
+
+            /* disconnect from IMAP server */
+            $imap->disconnect();
+
+            /* debug is reset when we reconnect, so dump this session now */
+            $imap->show_debug(false);
+
+            /* reconnect */
+            $imap->connect(array('username' => $username, 'password' => $password));
+            /* clear the current in memory cache in the $imap object */
+
+            /* load the cache data we dumped */
+            $imap->load_cache( $cache );
+
+            /* rerun the fetch command, should be served from cache. the debug
+             * output should contain a line like this:
+             *
+             *      Cache hit for INBOX with: UID SORT (ARRIVAL) US-ASCII ALL
+             */
+            if ( $imap->select_mailbox( 'INBOX' )['selected'] ) {
+                $imap->get_message_uids();
+            }
+        }
+    }
     /* disconnect from the IMAP server */
     $imap->disconnect();
 }
 
 /* dump session information */
-$imap->debug(false);
+$imap->show_debug(false);
 
 ?>
