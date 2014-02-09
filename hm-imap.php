0f936cbb7b6757740a397b297be74a8f8b8bcf9b
jason <jason@hastymail.org>
2014-02-09 10:35:47 -0600

    group cache related methods

--- hm-imap.php
+++ hm-imap.php
@@ -587,105 +587,6 @@ class Hm_IMAP_Base {
         }
     }
 
-    /**
-     * cache certain IMAP command return values for re-use
-     *
-     * @param $res array low level parsed IMAP response
-     * 
-     * @return array initial low level parsed IMAP response argument
-     */
-    protected function cache_return_val($res, $command) {
-        if (!$this->use_cache) {
-            return $res;
-        }
-        $command = str_replace(array("\r", "\n"), array(''), preg_replace("/^A\d+ /", '', $command));
-        if (strstr($command, 'LIST')) {
-            $this->cache_data['LIST'][$command] = $res;
-        }
-        elseif (strstr($command, 'LSUB')) {
-            $this->cache_data['LSUB'][$command] = $res;
-        }
-        elseif (strstr($command, 'NAMESPACE')) {
-            $this->cache_data['NAMESPACE'] = $res;
-        }
-        elseif ($this->selected_mailbox) {
-            $key = sha1((string) $this->selected_mailbox['name'].$this->selected_mailbox['detail']['uidvalidity'].
-                $this->selected_mailbox['detail']['uidnext'].$this->selected_mailbox['detail']['exists']);
-            $box = $this->selected_mailbox['name'];
-            if (isset($this->cache_keys[$box])) {
-                $old_key = $this->cache_keys[$box];
-            }
-            else {
-                $old_key = false;
-            }
-            if ($old_key && $old_key != $key) {
-                if (isset($this->cache_data[$old_key])) {
-                    unset($this->cache_data[$old_key]);
-                    $this->cache_data[$key] = array();
-                    $this->cache_keys[$box] = $key;
-                }
-            }
-            else {
-                if (!isset($this->cache_keys[$box])) {
-                    $this->cache_keys[$box] = $key;
-                }
-                if (!isset($this->cache_data[$key])) {
-                    $this->cache_data[$key] = array();
-                }
-            }
-            $this->cache_data[$key][$command] = $res;
-        }
-        /* TODO: prune cache */
-        return $res;
-    }
-
-    /**
-     * determine if the current command can be served from cache
-     *
-     * @param $command string IMAP command to check
-     * @param $check_only bool flag to avoid double logging
-     *
-     * @return mixed cached result or false if there is no cached data to use
-     */
-    public function check_cache( $command, $check_only=false ) {
-        if (!$this->use_cache) {
-            return false;
-        }
-        $command = str_replace(array("\r", "\n"), array(''), preg_replace("/^A\d+ /", '', $command));
-        $res = false;
-        $msg = '';
-        if (preg_match("/^LIST/ ", $command) && isset($this->cache_data['LIST'][$command])) {
-            $msg = 'Cache hit for: '.$command;
-            $res = $this->cache_data['LIST'][$command];
-        }
-        elseif (preg_match("/^LSUB /", $command) && isset($this->cache_data['LSUB'[$command]])) {
-            $msg = 'Cache hit for: '.$command;
-            $res = $this->cache_data['LSUB'][$command];
-        }
-        elseif (preg_match("/^NAMESPACE/", $command) && isset($this->cache_data['NAMESPACE'])) {
-            $msg = 'Cache hit for: '.$command;
-            $res = $this->cache_data['NAMESPACE'];
-        }
-        elseif ($this->selected_mailbox) {
-            $box = $this->selected_mailbox['name'];
-            if (isset($this->cache_keys[$box])) {
-                $key = $this->cache_keys[$box];
-                if (isset($this->cache_data[$key][$command])) {
-                    $msg = 'Cache hit for: '.$box.' with: '.$command;
-                    $res = $this->cache_data[$key][$command];
-                }
-            }
-        }
-        if ($check_only) {
-            return $res ? true : false;
-        }
-        if ($msg) {
-            $this->cached_response = true;
-            $this->debug[] = $msg;
-        }
-        return $res;
-    }
-
 }
 
 /* IMAP specific parsing routines */
@@ -1137,9 +1038,244 @@ class Hm_IMAP_Parser extends Hm_IMAP_Base {
     }
 
 }
+class Hm_IMAP_Cache extends Hm_IMAP_Parser {
+
+    /**
+     * update the cache untagged QRESYNC FETCH responses
+     *
+     * @param $data array low level parsed IMAP response segment
+     *
+     * @return int 1 if the cache was updated
+     */
+    protected function update_cache_data($data) {
+        $res = 0;
+        if (in_array('VANISHED', $data)) {
+            $uid = $this->get_adjacent_response_value($data, -1, 'VANISHED');
+            if ($this->is_clean($uid, 'uid')) {
+                if (isset($this->cache_keys[$this->selected_mailbox['name']])) {
+                    $key = $this->cache_keys[$this->selected_mailbox['name']];
+                    if (isset($this->cache_data[$key])) {
+                        foreach ($this->cache_data[$key] as $command => $result) {
+                            /* TODO: update other cached commands */
+                            if (strstr($command, 'UID FETCH')) {
+                                if (isset($result[$uid])) {
+                                    unset($this->cache_data[$key][$command][$uid]);
+                                    $this->debug[] = sprintf('Removed message from cache using QRESYNC response (uid: %s)', $uid);
+                                    $res = 1;
+                                }
+                            }
+                            elseif (strstr($command, 'UID SORT')) {
+                                $index = array_search($uid, $result);
+                                if ($index !== false) {
+                                    unset($this->cache_data[$key][$command][$index]);
+                                    $this->debug[] = sprintf('Removed message from cache using QRESYNC response (uid: %s)', $uid);
+                                    $res = 1;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        else {
+            $flags = array();
+            $uid = $this->get_adjacent_response_value($data, -1, 'UID');
+            if ($this->is_clean($uid, 'uid')) {
+                $flag_start = array_search('FLAGS', $data);
+                if ($flag_start !== false) {
+                    $flags = $this->get_flag_values(array_slice($data, $flag_start));
+                }
+            }
+            if ($uid) {
+                if (isset($this->cache_keys[$this->selected_mailbox['name']])) {
+                    $key = $this->cache_keys[$this->selected_mailbox['name']];
+                    if (isset($this->cache_data[$key])) {
+                        foreach ($this->cache_data[$key] as $command => $result) {
+                            /* TODO: update other cached commands */
+                            if (strstr($command, 'UID FETCH')) {
+                                if (isset($result[$uid]['flags'])) {
+                                    $this->cache_data[$key][$command][$uid]['flags'] = implode(' ', $flags);
+                                    $this->debug[] = sprintf('Updated cache data from QRESYNC response (uid: %s)', $uid);
+                                    $res = 1;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return $res;
+    }
+
+    /**
+     * cache certain IMAP command return values for re-use
+     *
+     * @param $res array low level parsed IMAP response
+     * 
+     * @return array initial low level parsed IMAP response argument
+     */
+    protected function cache_return_val($res, $command) {
+        if (!$this->use_cache) {
+            return $res;
+        }
+        $command = str_replace(array("\r", "\n"), array(''), preg_replace("/^A\d+ /", '', $command));
+        if (strstr($command, 'LIST')) {
+            $this->cache_data['LIST'][$command] = $res;
+        }
+        elseif (strstr($command, 'LSUB')) {
+            $this->cache_data['LSUB'][$command] = $res;
+        }
+        elseif (strstr($command, 'NAMESPACE')) {
+            $this->cache_data['NAMESPACE'] = $res;
+        }
+        elseif ($this->selected_mailbox) {
+            $key = sha1((string) $this->selected_mailbox['name'].$this->selected_mailbox['detail']['uidvalidity'].
+                $this->selected_mailbox['detail']['uidnext'].$this->selected_mailbox['detail']['exists']);
+            $box = $this->selected_mailbox['name'];
+            if (isset($this->cache_keys[$box])) {
+                $old_key = $this->cache_keys[$box];
+            }
+            else {
+                $old_key = false;
+            }
+            if ($old_key && $old_key != $key) {
+                if (isset($this->cache_data[$old_key])) {
+                    unset($this->cache_data[$old_key]);
+                    $this->cache_data[$key] = array();
+                    $this->cache_keys[$box] = $key;
+                }
+            }
+            else {
+                if (!isset($this->cache_keys[$box])) {
+                    $this->cache_keys[$box] = $key;
+                }
+                if (!isset($this->cache_data[$key])) {
+                    $this->cache_data[$key] = array();
+                }
+            }
+            $this->cache_data[$key][$command] = $res;
+        }
+        /* TODO: prune cache */
+        return $res;
+    }
+
+    /**
+     * determine if the current command can be served from cache
+     *
+     * @param $command string IMAP command to check
+     * @param $check_only bool flag to avoid double logging
+     *
+     * @return mixed cached result or false if there is no cached data to use
+     */
+    protected function check_cache( $command, $check_only=false ) {
+        if (!$this->use_cache) {
+            return false;
+        }
+        $command = str_replace(array("\r", "\n"), array(''), preg_replace("/^A\d+ /", '', $command));
+        $res = false;
+        $msg = '';
+        if (preg_match("/^LIST/ ", $command) && isset($this->cache_data['LIST'][$command])) {
+            $msg = 'Cache hit for: '.$command;
+            $res = $this->cache_data['LIST'][$command];
+        }
+        elseif (preg_match("/^LSUB /", $command) && isset($this->cache_data['LSUB'[$command]])) {
+            $msg = 'Cache hit for: '.$command;
+            $res = $this->cache_data['LSUB'][$command];
+        }
+        elseif (preg_match("/^NAMESPACE/", $command) && isset($this->cache_data['NAMESPACE'])) {
+            $msg = 'Cache hit for: '.$command;
+            $res = $this->cache_data['NAMESPACE'];
+        }
+        elseif ($this->selected_mailbox) {
+            $box = $this->selected_mailbox['name'];
+            if (isset($this->cache_keys[$box])) {
+                $key = $this->cache_keys[$box];
+                if (isset($this->cache_data[$key][$command])) {
+                    $msg = 'Cache hit for: '.$box.' with: '.$command;
+                    $res = $this->cache_data[$key][$command];
+                }
+            }
+        }
+        if ($check_only) {
+            return $res ? true : false;
+        }
+        if ($msg) {
+            $this->cached_response = true;
+            $this->debug[] = $msg;
+        }
+        return $res;
+    }
+
+    /**
+     * invalidate parts of the data cache
+     *
+     * @param $type string can be one of LIST, LSUB, ALL, or a mailbox name
+     *
+     * @return void
+     */
+    public function bust_cache($type) {
+        if (!$this->use_cache) {
+            return;
+        }
+        switch($type) {
+            case 'LIST':
+                if (isset($this->cache_data['LIST'])) {
+                    unset($this->cache_data['LIST']);
+                    $this->debug[] = 'cache busted: '.$type;
+                }
+                break;
+            case 'LSUB':
+                if (isset($this->cache_data['LSUB'])) {
+                    unset($this->cache_data['LSUB']);
+                    $this->debug[] = 'cache busted: '.$type;
+                }
+                break;
+            case 'ALL':
+                $this->cache_keys = array();
+                $this->cache_data = array();
+                $this->debug[] = 'cache busted: '.$type;
+                break;
+            default:
+                if (isset($this->cache_keys[$type])) {
+                    if(isset($this->cache_data[$this->cache_keys[$type]])) {
+                        unset($this->cache_data[$this->cache_keys[$type]]);
+                    }
+                    unset($this->cache_keys[$type]);
+                    $this->debug[] = 'cache busted: '.$type;
+                }
+                break;
+        }
+    }
+
+    /**
+     * output a string version of the cache that can be re-used
+     *
+     * @return string serialized version of the cache data
+     */
+    public function dump_cache() {
+        return serialize(array($this->cache_keys, $this->cache_data));
+    }
+
+    /**
+     * load cache data from the output of dump_cache()
+     *
+     * @param $data string serialized cache data from dump_cache()
+     * @return void
+     */
+    public function load_cache($data) {
+        $data = unserialize($data);
+        if (isset($data[0]) && isset($data[1])) {
+            $this->cache_keys = $data[0];
+            $this->cache_data = $data[1];
+            $this->debug[] = 'Cache loaded: '.count($this->cache_data);
+        }
+    }
+
+
+}
 
 /* public interface to IMAP commands */
-class Hm_IMAP extends Hm_IMAP_Parser {
+class Hm_IMAP extends Hm_IMAP_Cache {
 
     /* config */
     public $max_read = false;
@@ -1266,73 +1402,6 @@ class Hm_IMAP extends Hm_IMAP_Parser {
             $qresync = true;
         }
         return array($qresync, $attributes);
-    }
-
-    /**
-     * update the cache untagged QRESYNC FETCH responses
-     *
-     * @param $data array low level parsed IMAP response segment
-     *
-     * @return int 1 if the cache was updated
-     */
-    private function update_cache_data($data) {
-        $res = 0;
-        if (in_array('VANISHED', $data)) {
-            $uid = $this->get_adjacent_response_value($data, -1, 'VANISHED');
-            if ($this->is_clean($uid, 'uid')) {
-                if (isset($this->cache_keys[$this->selected_mailbox['name']])) {
-                    $key = $this->cache_keys[$this->selected_mailbox['name']];
-                    if (isset($this->cache_data[$key])) {
-                        foreach ($this->cache_data[$key] as $command => $result) {
-                            /* TODO: update other cached commands */
-                            if (strstr($command, 'UID FETCH')) {
-                                if (isset($result[$uid])) {
-                                    unset($this->cache_data[$key][$command][$uid]);
-                                    $this->debug[] = sprintf('Removed message from cache using QRESYNC response (uid: %s)', $uid);
-                                    $res = 1;
-                                }
-                            }
-                            elseif (strstr($command, 'UID SORT')) {
-                                $index = array_search($uid, $result);
-                                if ($index !== false) {
-                                    unset($this->cache_data[$key][$command][$index]);
-                                    $this->debug[] = sprintf('Removed message from cache using QRESYNC response (uid: %s)', $uid);
-                                    $res = 1;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        else {
-            $flags = array();
-            $uid = $this->get_adjacent_response_value($data, -1, 'UID');
-            if ($this->is_clean($uid, 'uid')) {
-                $flag_start = array_search('FLAGS', $data);
-                if ($flag_start !== false) {
-                    $flags = $this->get_flag_values(array_slice($data, $flag_start));
-                }
-            }
-            if ($uid) {
-                if (isset($this->cache_keys[$this->selected_mailbox['name']])) {
-                    $key = $this->cache_keys[$this->selected_mailbox['name']];
-                    if (isset($this->cache_data[$key])) {
-                        foreach ($this->cache_data[$key] as $command => $result) {
-                            /* TODO: update other cached commands */
-                            if (strstr($command, 'UID FETCH')) {
-                                if (isset($result[$uid]['flags'])) {
-                                    $this->cache_data[$key][$command][$uid]['flags'] = implode(' ', $flags);
-                                    $this->debug[] = sprintf('Updated cache data from QRESYNC response (uid: %s)', $uid);
-                                    $res = 1;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        return $res;
     }
 
     /**
@@ -2648,71 +2717,6 @@ class Hm_IMAP extends Hm_IMAP_Parser {
             }
         }
         return $res;
-    }
-
-    /**
-     * invalidate parts of the data cache
-     *
-     * @param $type string can be one of LIST, LSUB, ALL, or a mailbox name
-     *
-     * @return void
-     */
-    public function bust_cache($type) {
-        if (!$this->use_cache) {
-            return;
-        }
-        switch($type) {
-            case 'LIST':
-                if (isset($this->cache_data['LIST'])) {
-                    unset($this->cache_data['LIST']);
-                    $this->debug[] = 'cache busted: '.$type;
-                }
-                break;
-            case 'LSUB':
-                if (isset($this->cache_data['LSUB'])) {
-                    unset($this->cache_data['LSUB']);
-                    $this->debug[] = 'cache busted: '.$type;
-                }
-                break;
-            case 'ALL':
-                $this->cache_keys = array();
-                $this->cache_data = array();
-                $this->debug[] = 'cache busted: '.$type;
-                break;
-            default:
-                if (isset($this->cache_keys[$type])) {
-                    if(isset($this->cache_data[$this->cache_keys[$type]])) {
-                        unset($this->cache_data[$this->cache_keys[$type]]);
-                    }
-                    unset($this->cache_keys[$type]);
-                    $this->debug[] = 'cache busted: '.$type;
-                }
-                break;
-        }
-    }
-
-    /**
-     * output a string version of the cache that can be re-used
-     *
-     * @return string serialized version of the cache data
-     */
-    public function dump_cache() {
-        return serialize(array($this->cache_keys, $this->cache_data));
-    }
-
-    /**
-     * load cache data from the output of dump_cache()
-     *
-     * @param $data string serialized cache data from dump_cache()
-     * @return void
-     */
-    public function load_cache($data) {
-        $data = unserialize($data);
-        if (isset($data[0]) && isset($data[1])) {
-            $this->cache_keys = $data[0];
-            $this->cache_data = $data[1];
-            $this->debug[] = 'Cache loaded: '.count($this->cache_data);
-        }
     }
 
     /**
